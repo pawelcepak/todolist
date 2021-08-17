51a4463e66d79b6af43bbbc306c3ede447a8ceb5
Pawel Cepak <53912041+pawelcepak@users.noreply.github.com>
2021-08-17 21:55:49 +0200

    Edit files

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
 let addTaskText = document.querySelector('#ati');
-const btnAddTask = document.querySelector('#btnAddTask');
+const btnAddTask = document.querySelector('#addTITask');
 const taskProducts = document.querySelector('.taskProducts');
 
 const errorAddTask = document.querySelector('.errorAddTask');
 
-
-
-
 btnAddTask.addEventListener('click', () => {
     errorAddTask.style.display = 'none';
+
     // create div element (addTask)
     let newTaskItem = document.createElement('div');
     newTaskItem.setAttribute('class', 'taskProduct');
-
     // create p element (addTask)
     let newPItem = document.createElement('p');
     newPItem.setAttribute('class', 'taskProductP');
-
-
 
     if (addTaskText.value === "") {
         errorAddTask.style.display = 'block';
@@ -27,7 +22,6 @@ btnAddTask.addEventListener('click', () => {
         newTaskItem.appendChild(newPItem);
         taskProducts.appendChild(newTaskItem);
     }
-
 
     addTaskText.value = "";
 });
